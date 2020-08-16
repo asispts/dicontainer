a893bfca3de9e596a0a565afffb037b6a0d21868
Asis Pattisahusiwa <pattisahusiwa@users.noreply.github.com>
2020-08-16 14:25:15 +0900

    Use specific exception class name

--- tests/Units/FailedTest.php
+++ tests/Units/FailedTest.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 
-use Psr\Container\ContainerExceptionInterface;
+use Xynha\Container\ContainerException;
+use Xynha\Container\NotFoundException;
 use Xynha\Tests\AbstractTestCase;
 
 final class FailedTest extends AbstractTestCase
@@ -9,7 +10,7 @@ final class FailedTest extends AbstractTestCase
     public function testInterface()
     {
         $msg = sprintf('Class or rule `%s` is not found or it is an interface', 'ArrayAccess');
-        $this->expectException(ContainerExceptionInterface::class);
+        $this->expectException(NotFoundException::class);
         $this->expectExceptionMessage($msg);
 
         $this->dic->get('ArrayAccess');
@@ -17,7 +18,7 @@ final class FailedTest extends AbstractTestCase
 
     public function testAbstractClass()
     {
-        $this->expectException(ContainerExceptionInterface::class);
+        $this->expectException(ContainerException::class);
         $this->expectExceptionMessage('Cannot instantiate abstract class AbstractFailed');
 
         $this->dic->get('AbstractFailed');
@@ -25,7 +26,7 @@ final class FailedTest extends AbstractTestCase
 
     public function testPrivateConstructor()
     {
-        $this->expectException(ContainerExceptionInterface::class);
+        $this->expectException(ContainerException::class);
         $this->expectExceptionMessage('Access to non-public constructor of class PrivateFailed');
 
         $this->dic->get('PrivateFailed');
@@ -33,7 +34,7 @@ final class FailedTest extends AbstractTestCase
 
     public function testProtectedConstructor()
     {
-        $this->expectException(ContainerExceptionInterface::class);
+        $this->expectException(ContainerException::class);
         $this->expectExceptionMessage('Access to non-public constructor of class ProtectedFailed');
 
         $this->dic->get('ProtectedFailed');
