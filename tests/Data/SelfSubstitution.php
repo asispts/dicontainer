e81ca90f4f299a6cbe895faf6eefe912242a072c
Asis Pattisahusiwa <pattisahusiwa@users.noreply.github.com>
2020-09-04 09:01:51 +0900

    Add get DiContainer test

---
+++ tests/Data/SelfSubstitution.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+class DicDependant
+{
+
+    public $dic;
+
+    public function __construct(\Psr\Container\ContainerInterface $dic)
+    {
+        $this->dic = $dic;
+    }
+}
