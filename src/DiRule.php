0f95c4dececc594f1330737dad1a0e8c5bd35bfd
Asis Pattisahusiwa <pattisahusiwa@users.noreply.github.com>
2020-09-08 21:31:08 +0900

    Add document copyright information

--- src/DiRule.php
+++ src/DiRule.php
@@ -1,5 +1,12 @@
 <?php declare(strict_types=1);
 
+/**
+ * This file is part of xynha/dicontainer package.
+ *
+ * @author Asis Pattisahusiwa <asis.pattisahusiwa@gmail.com>
+ * @copyright 2020 Asis Pattisahusiwa
+ * @license https://github.com/pattisahusiwa/dicontainer/blob/master/LICENSE Apache-2.0 License
+ */
 namespace Xynha\Container;
 
 final class DiRule
